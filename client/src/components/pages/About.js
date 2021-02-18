4bdb7c264ae205f1bca6f87031e865e39c0923a2
shelton gayles <sheltong@jakks.net>
2021-02-17 18:35:58 -0800

    front end working with nav and basic routes

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>This is a full stack React demo based on Jakksip</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong>1.0.0
+      </p>
+    </div>
+  );
+}
+
+export default About;
