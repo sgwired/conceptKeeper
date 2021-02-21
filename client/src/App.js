9a8845e332ad3283e9ef5214289ba3988d398c47
shelton gayles <sheltong@jakks.net>
2021-02-21 12:04:09 -0800

    started auth context

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ConceptState from './context/concept/ConceptState';
+import AuthState from './context/auth/AuthState';
 
 function App() {
   return (
-    <ConceptState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ConceptState>
+    <AuthState>
+      <ConceptState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ConceptState>
+    </AuthState>
   );
 }
 
