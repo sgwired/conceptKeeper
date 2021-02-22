9b28bb1edc5d81eeb7204ab1d9215eec2c7757b1
shelton gayles <sheltong@jakks.net>
2021-02-22 15:05:24 -0800

    fixed error msg typo

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.post(
     try {
       let user = await User.findOne({ email: email });
       if (user) {
-        res.status(400).json({ msg: 'User alreay exists' });
+        res.status(400).json({ msg: 'User already exists' });
       }
       user = new User({
         name,
