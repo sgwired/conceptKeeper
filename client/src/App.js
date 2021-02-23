09b2af86487f13cc90a068001eb25233a03bd750
shelton gayles <sheltong@jakks.net>
2021-02-22 16:53:07 -0800

    private route working after login

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import ConceptState from './context/concept/ConceptState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
 import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -27,7 +28,7 @@ function App() {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, loading } = authContext;
+
+  if (!isAuthenticated) {
+    console.log('not is auth: ', isAuthenticated);
+  }
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
